fe35c6886bcfeb709d05cbdaf21064b4a30f1532
nishatalam24 <tajnoor2443@gmail.com>
2025-12-04 00:15:01 +0530

    client changes

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -39,18 +39,31 @@ function Navbar() {
   const [subLinks, setSubLinks] = useState([])
   const [loading, setLoading] = useState(false)
 
-  useEffect(() => {
+
+   useEffect(() => {
     ;(async () => {
       setLoading(true)
       try {
         const res = await apiConnector("GET", categories.CATEGORIES_API)
-        setSubLinks(res.data.data)
+        setSubLinks(res.data.data || [])
       } catch (error) {
         console.log("Could not fetch Categories.", error)
       }
       setLoading(false)
     })()
   }, [])
+  // useEffect(() => {
+  //   ;(async () => {
+  //     setLoading(true)
+  //     try {
+  //       const res = await apiConnector("GET", categories.CATEGORIES_API)
+  //       setSubLinks(res.data.data)
+  //     } catch (error) {
+  //       console.log("Could not fetch Categories.", error)
+  //     }
+  //     setLoading(false)
+  //   })()
+  // }, [])
 
   // console.log("sub links", subLinks)
 
@@ -87,9 +100,9 @@ function Navbar() {
                       <BsChevronDown />
                       <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
                         <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
-                        {loading ? (
+                         {loading ? (
                           <p className="text-center">Loading...</p>
-                        ) : subLinks.length ? (
+                        ) : subLinks?.length ? (
                           <>
                             {subLinks
                               ?.filter(
